4f6d0b6e15460f6643b6736d74188beef15faad8
김원이 <codeliner96@fixelsoft.com>
2022-08-09 17:55:02 +0900

    Initial commit

---
+++ test/cute-infra-constructor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CuteInfraConstructor from '../lib/cute-infra-constructor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cute-infra-constructor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CuteInfraConstructor.CuteInfraConstructorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
