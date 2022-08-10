7279f0715074b161a7d6c1a630d34617a6ceb82d
김원이 <codeliner96@fixelsoft.com>
2022-08-10 12:11:13 +0900

    Created a cute infra structor app.

--- lib/cute-infra-constructor-stack.ts
+++ lib/cute-infra-constructor-stack.ts
@@ -1,16 +1,45 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct } from "constructs";
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+} from "aws-cdk-lib/aws-cloudfront";
 
 export class CuteInfraConstructorStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Defined origin access identity.
+    const originAccessIdentity = new OriginAccessIdentity(this, "cute-oai");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CuteInfraConstructorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Defined s3 bucket.
+    const s3BucketSource = new Bucket(this, "cute-bucket", {
+      bucketName: "cute-bucket",
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Add s3 bucket policy.
+    s3BucketSource.addToResourcePolicy(
+      new PolicyStatement({
+        sid: "PolicyWhichCloudFrontAccessibleToBucket",
+        effect: Effect.ALLOW,
+        actions: ["s3:GetObject"],
+        resources: [`${s3BucketSource.bucketArn}/*`],
+        principals: [originAccessIdentity.grantPrincipal],
+      })
+    );
+
+    // Defined cloudfront distribution.
+    new CloudFrontWebDistribution(this, "cute-cloudfront", {
+      originConfigs: [
+        {
+          s3OriginSource: { s3BucketSource, originAccessIdentity },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+    });
   }
 }
